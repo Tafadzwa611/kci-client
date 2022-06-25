c157ebf3ccefa69db6b613497200b2359b037f62
Tafadzwa Kuno <tkuno157@gmail.com>
2022-06-25 21:37:55 +0200

    fixed login class error

--- src/components/Login/NormalLogin.js
+++ src/components/Login/NormalLogin.js
@@ -9,7 +9,7 @@ function NormalLogin(props) {
             <div>
               <form autoComplete='off' className='card-body-login' onSubmit={props.login}>
                 <div className='w-75-login m-auto-login'>
-                  <h4 className='text-dark-50-login text-center-login mt-0-login font-weight-bold-login'>Sign In</h4> 
+                  <h4 className='text-dark-50-login text-center-login mt-0-login font-weight-bold-login'>Log In</h4> 
                   <p className='text-muted-login mb-3-login text-center-login'>Enter your email address and password</p>
                 </div>
                 <div style={{fontSize: 12, color: 'red'}}>{props.errors.non_field_errors}</div>
