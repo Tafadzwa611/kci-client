5e29c611bf6a8c1197d62ffa07683841798610ea
Tafadzwa Kuno <tkuno157@gmail.com>
2022-07-14 13:05:01 +0200

    currency update

---
+++ src/components/Users/CreateCurrencyModal.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import { makeRequest } from '../../utils/utils';
+
+const initialState = {fullname: '', shortname: '', date_created: ''};
+
+function CreateCurrencyModal({open, setOpen, setCurrencies}) {
+  const [cur, setCur] = useState(initialState);
+  const [serverErrors, setServerErrors] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const handleExpenseCurrencyChange = (e) => {
+    const { name, value } = e.target;
+    setCur({ ...cur, [name]: value });
+    validate(e)
+  };
+
+  const validate = (evt) => {
+    if (evt.target.required && evt.target.value === '') {
+      setErrors(curr => {
+        return {...curr, [evt.target.name]: 'This field is required'}
+      })
+    }else {
+      setErrors(curr => {
+        return {...curr, [evt.target.name]: ''}
+      })
+    }
+  }
+
+  const submit = async () => {
+    const body = {
+      fullname: cur.fullname,
+      shortname: cur.shortname,
+      date_created: cur.date_created,
+    };
+    const response = await makeRequest.post('/usersapi/add_currency/', body, {timeout: 8000});
+    if (response.ok) {
+      const data = await response.json();
+      setCurrencies(curr => [data, ...curr])
+      setOpen(curr => !curr)
+      setCur(initialState)
+      return data
+    }
+    const errors = await response.json();
+    console.log(errors);
+    setServerErrors(errors);
+    window.scrollTo(0, 0);
+  }
+
+
+  return (
+    <div className={open ? 'modal fade show' : 'modal fade'} style={{display: open ? 'block' : 'none'}}>
+      <div className='modal-dialog modal-lg modal-dialog-scrollable'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <label className="form-title">[ Add Currency ]</label>
+            <button type='button' className='close' onClick={(e) => setOpen(curr => !curr)}><span aria-hidden='true'>&times;</span></button>
+          </div>
+          <div className='modal-body'>
+
+            <div className='row custom-background' style={{marginTop: '15px'}}>
+              <label className='form-label'>Fullname<span style={{color: 'red'}}>*</span></label>
+              <div className='col-9'>
+                <input name='fullname' type='text' className='custom-select-form' autoComplete='new-password' onFocus={validate} onChange={handleExpenseCurrencyChange} value={cur.fullname} required/>
+                <p style={{color: 'red'}}>{errors['fullname']}</p>
+              </div>
+            </div>
+
+            <div className='row custom-background' style={{marginTop: '15px'}}>
+              <label className='form-label'>Shortname<span style={{color: 'red'}}>*</span></label>
+              <div className='col-9'>
+                <input name='shortname' type='text' className='custom-select-form' autoComplete='new-password' onFocus={validate} onChange={handleExpenseCurrencyChange} value={cur.shortname} required/>
+                <p style={{color: 'red'}}>{errors['shortname']}</p>
+              </div>
+            </div>
+
+            <div className='row custom-background' style={{marginTop: '15px'}}>
+              <label className='form-label'>Date Created<span style={{color: 'red'}}>*</span></label>
+              <div className='col-9'>
+                <input name='date_created' type='date' className='custom-select-form' autoComplete='new-password' onFocus={validate} onChange={handleExpenseCurrencyChange} value={cur.date_created} required/>
+                <p style={{color: 'red'}}>{errors['date_created']}</p>
+              </div>
+            </div>
+
+            <div className="row">
+                <p><label className='form-label'><span style={{color: 'red'}}>*</span>required field</label></p>
+            </div>
+
+          </div>
+
+          <div className='modal-footer justify-content-between'>
+            <button type='button' className='btn btn-default' onClick={(e) => setOpen(curr => !curr)}>Close</button>
+            <button type='submit' className='btn btn-info' onClick={submit}>
+              Add Currency
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateCurrencyModal;
--- src/components/Users/Currencies.js
+++ src/components/Users/Currencies.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { NavLink } from 'react-router-dom';
+import { makeRequest } from '../../utils/utils';
+import CreateCurrencyModal from './CreateCurrencyModal';
 
 const Currencies = () => {
+
+    const [currencies, setCurrencies] = useState([])
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        getCurrencies()
+    }, []);
+
+    const getCurrencies = async () => {
+        window.scrollTo(0, 0);
+        document.title = 'Currencies';
+        const data = await fetchCurrencies();
+        setCurrencies(data);
+    };
+
+    async function fetchCurrencies() {
+        try {
+            const response = await makeRequest.get(`/usersapi/currencieslist/`, {timeout: 8000});
+            if (response.ok) {
+                const currency_types = await response.json();
+                return currency_types;
+            }else {
+                const error = await response.json();
+                console.log(error);
+            }
+        }catch(error) {
+            console.log(error);
+        }
+    }
+
+    if (currencies == null) {
+        return <div>Loading ...</div>;
+    } 
+
     return (
-        <div className="card slide">
+        <div className="card font-12">
             <div className="card-body">
                 <h5 className="table-heading">Currencies</h5>
+                <CreateCurrencyModal open={open} setOpen={setOpen} setCurrencies={setCurrencies} />
                 <div style={{margin:"20px 0"}}>
-                    <NavLink className="btn btn-success" to="/addcurrency">Add Currency</NavLink><br/>
+                    <button type='button' className='btn btn-success' onClick={(e) => setOpen(curr => !curr)}>Add Currency</button>
                 </div>
                 <div className="table-responsive font-12">
                     <table className="table table-hover">
@@ -15,15 +52,13 @@ const Currencies = () => {
                             <tr className="bg-gray">
                                 <th>Full_Name</th>
                                 <th>ISO Code</th>
-                            </tr>                      
-                            <tr className="table-row">
-                                <td>US Dollar</td>
-                                <td>USD</td>         
-                            </tr>
-                            <tr className="table-row">
-                                <td>Zimbabwean Dollar</td>
-                                <td>ZWD</td>         
-                            </tr>
+                            </tr>  
+                            {currencies.map((currency) => (
+                                <tr className="table-row" key={currency.id}>
+                                    <td>{currency.fullname}</td>
+                                    <td>{currency.shortname}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
