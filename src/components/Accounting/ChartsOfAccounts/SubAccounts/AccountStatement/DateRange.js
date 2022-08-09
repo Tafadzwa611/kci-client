324147c145065d2f8e7f87d699d368948de907ee
Tafadzwa Kuno <tkuno157@gmail.com>
2022-08-09 15:24:36 +0200

    assets sub accounts update

--- src/components/Accounting/ChartsOfAccounts/SubAccounts/AccountStatement/DateRange.js
+++ src/components/Accounting/ChartsOfAccounts/SubAccounts/AccountStatement/DateRange.js
@@ -33,28 +33,30 @@ const DateRange = ({
     return (
         <div className="col-12 font-12 balance_sheet_date_range text-light" style={{border:"none", padding:"0 1.5rem"}}>
             <form>
-                <div className="view_search_container online__applications font-13 acc__statement-search" style={{border:"none", padding:"0", marginBottom:'1.5rem', display:"flex", justifyContent:"space-between", flexDirection:"row"}}>
+                <div className="view_search_container online__applications font-13 acc__statement-search" style={{border:"none", padding:"0", marginBottom:'1.5rem', display:"grid", gridTemplateColumns:"80% auto", columnGap:"10px"}}>
                     
-                    <div className="row-payments-container" style={{width:"35%"}}>
-                        <input
-                            type='date'
-                            className='custom-select-form row-form'
-                            value={minDate}
-                            onKeyDown={(e) => e.preventDefault()}
-                            onChange={(date) => setMinDate(date.target.value)}
-                            style={{width:"100%"}}
-                        />
-                    </div>
+                    <div style={{display:"flex", justifyContent:"space-between"}}>
+                        <div className="row-payments-container" style={{width:"45%"}}>
+                            <input
+                                type='date'
+                                className='custom-select-form row-form'
+                                value={minDate}
+                                onKeyDown={(e) => e.preventDefault()}
+                                onChange={(date) => setMinDate(date.target.value)}
+                                style={{width:"100%"}}
+                            />
+                        </div>
 
-                    <div className="row-payments-container" style={{width:"35%"}}>
-                        <input
-                            type='date'
-                            className='custom-select-form row-form'
-                            value={maxDate}
-                            onKeyDown={(e) => e.preventDefault()}
-                            onChange={(date) => setMaxDate(date.target.value)}
-                            style={{width:"100%"}}
-                        />
+                        <div className="row-payments-container" style={{width:"45%"}}>
+                            <input
+                                type='date'
+                                className='custom-select-form row-form'
+                                value={maxDate}
+                                onKeyDown={(e) => e.preventDefault()}
+                                onChange={(date) => setMaxDate(date.target.value)}
+                                style={{width:"100%"}}
+                            />
+                        </div>
                     </div>
 
                     <div style={{width:"20%"}}>
