2cd8348ef2fd70531a22258f53dae7eefade87c8
Tafadzwa Kuno <tkuno157@gmail.com>
2022-08-11 12:45:59 +0200

    otherincome table width added on columns

--- src/components/OtherIncome/OtherIncomeFooter.js
+++ src/components/OtherIncome/OtherIncomeFooter.js
@@ -4,11 +4,11 @@ const OtherIncomeFooter = ({otherincomes, totalCount, nextPageNumber, loadMoreCl
     return (
         <>
             {nextPageNumber === null ?
-                <div className="all-data-loaded" style={{padding:"1rem 0"}}>
+                <div className="all-data-loaded text-light" style={{padding:"1rem 0"}}>
                     <i className="uil uil-exclamation-triangle"></i> 
                     <span>All other incomes have been loaded</span>
                 </div> :
-                <div className="load-more-container card-body view_expenses">
+                <div className="load-more-container card-body view_expenses text-light">
                     <p className="load-more-container-left">
                         Showing {otherincomes.length} of {totalCount} other income.
                     </p>
