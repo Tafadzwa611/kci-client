3592410fd831956a8f90a7a37b38936648296a07
Tafadzwa Kuno <tkuno157@gmail.com>
2022-08-13 23:02:20 +0200

    clients update

--- src/components/Clients/ClientsList/ClientsList.js
+++ src/components/Clients/ClientsList/ClientsList.js
@@ -1,31 +1,139 @@
-import React from 'react';
-import ClientList from '../ClientList';
-import ClientSearchForm from '../ClientSearchForm';
+import React, { useState, useEffect, useRef } from 'react';
+import { makeRequest } from '../../../utils/utils';
+import ClientsTable from './ClientsTable';
+import Filter from './Filter';
 
+function ClientsList() {
+  const [clients, setClients] = useState(null);
+  const [branches, setBranches] = useState(null);
+  const [branchIds, setBranchIds] = useState(null);
+  const [nextPageNumber, setNextPageNumber] = useState(null);
+  const [totalCount, setTotalCount] = useState(0);
+  const [searchStr, setSearchStr] = useState('');
+  const [minRegDate, setMinRegDate] = useState('');
+  const [maxRegDate, setMaxRegDate] = useState('');
+  const [minDob, setMinDob] = useState('');
+  const [maxDob, setMaxDob] = useState('');
+  const [typeOfClient, setTypeOfClient] = useState('');
+  const [gender, setGender] = useState('');
+  const [loadingMore, setLoadingMore] = useState(false);
+  
+  const pageNum = useRef(1);
 
-const ClientsList = () => {
+  useEffect(async () => {
+    window.scrollTo(0, 0);
+    const data = await fetchClients();
+    const branches = await fetchBranches();
+    setClients(data.clients);
+    setBranches(branches);
+    setTotalCount(data.count);
+  }, []);
 
-    return (
-        <div className="font-12">
-                <ClientSearchForm/>
+  async function fetchClients() {
+    try {
+      const url = getUrl();
+      const response = await makeRequest.get(url, {timeout: 8000});
+      if (response.ok) {
+        const json_res = await response.json();
+        setNextPageNumber(json_res.next_page_num);
+        return json_res;
+      }else {
+        const error = await response.json();
+        console.log(error);
+      }
+    }catch(error) {
+      console.log(error);
+    }
+  }
 
-                <ClientList />
-                <div className="table-footer-container card-body clients_table">
-                    <div className="table-footer-up">
-                        <p className="load-more-container-left">
-                            Showing 10 of 1200
-                        </p>
-                        <button className="btn btn-info">Load More</button>
-                    </div>
-                    {/* <div className="all-data-loaded">
-                        <i class="uil uil-exclamation-triangle"></i> 
-                        <span>All clients have been loaded</span>
-                    </div> */}
-                </div>
+  async function fetchBranches() {
+    try {
+      const response = await makeRequest.get('/usersapi/get-branches/', {timeout: 8000});
+      if (response.ok) {
+        const json_res = await response.json();
+        return json_res.results;
+      }else {
+        const error = await response.json();
+        console.log(error);
+      }
+    }catch(error) {
+      console.log(error);
+    }
+  }
 
+  function getUrl() {
+    let url = `/clientsapi/clients/?page_num=${pageNum.current}`;
+    if (branchIds !== null) {
+      branchIds.forEach(id => (url += `&branch_ids=${id}`));
+    }
+    if (searchStr !== '') {
+      url += `&search_str=${searchStr}`;
+    }
+    if (minRegDate !== '') {
+      url += `&min_reg_date=${minRegDate}`;
+    }
+    if (maxRegDate !== '') {
+      url += `&max_reg_date=${maxRegDate}`;
+    }
+    if (minDob !== '') {
+      url += `&min_dob=${minDob}`;
+    }
+    if (maxDob !== '') {
+      url += `&max_dob=${maxDob}`;
+    }
+    if (typeOfClient !== '') {
+      url += `&type_of_client=${typeOfClient}`;
+    }
+    if (gender !== '') {
+      url += `&gender=${gender}`;
+    }
+    return url
+  }
 
-        </div>
-    );
+  if (clients===null || branches===null) {
+    return <div>Loading</div>
+  }
+
+  const loadMore = async (evt) => {
+    evt.preventDefault();
+    pageNum.current += 1;
+    const data = await fetchClients();
+    setClients(curr => [...curr,...data.clients]);
+  }
+
+  const onSubmit = async (evt) => {
+    evt.preventDefault();
+    pageNum.current = 1;
+    const data = await fetchClients();
+    setTotalCount(data.count);
+    setClients(data.clients);
+  }
+
+  return (
+    <>
+        <Filter
+            minRegDate={minRegDate}
+            setMinRegDate={setMinRegDate}
+            maxRegDate={maxRegDate}
+            minDob={minDob}
+            setMinDob={setMinDob}
+            maxDob={maxDob}
+            setMaxDob={setMaxDob}
+            setMaxRegDate={setMaxRegDate}
+            searchStr={searchStr}
+            setSearchStr={setSearchStr}
+            branches={branches}
+            setBranchIds={setBranchIds}
+            typeOfClient={typeOfClient}
+            setTypeOfClient={setTypeOfClient}
+            gender={gender}
+            setGender={setGender}
+            onSubmit={onSubmit}
+        />
+        <div style={{paddingTop: '1.5rem'}}></div>
+        <ClientsTable clients={clients} totalCount={totalCount} nextPageNumber={nextPageNumber} loadMoreClients={loadMore} loadingMore={loadingMore}/>
+    </>
+  )
 }
 
 export default ClientsList;
---
+++ src/components/Clients/ClientsList/ClientsTable.js
@@ -0,0 +1,55 @@
+import React from 'react';
+// import {useHistory} from 'react-router-dom';
+// import { convertDate, getAge } from '../client_details/company/utils';
+import Footer from './Footer';
+
+function ClientsTable({clients, nextPageNumber, loadMoreClients, totalCount, loadingMore}) {
+//   let history = useHistory();
+
+  const goToClientDetails = (evt) => {
+    evt.preventDefault();
+    // history.push(`/cDetails/${evt.target.id}`);
+  }
+
+  return (
+    <>
+        <div className='table-responsive font-12'>
+            <table className='table table-centered table-hover'>
+                <thead className="thead-light">
+                    <tr>
+                        <th style={{textAlign:"start"}}>Client_Number</th>
+                        <th style={{textAlign:"start"}}>Full_Name</th>
+                        <th style={{textAlign:"start"}}>Type_Of_client</th>
+                        <th style={{textAlign:"start"}}>Contact</th>
+                        <th style={{textAlign:"start"}}>Gender</th>
+                        <th style={{textAlign:"start"}}>Registration_Date</th>
+                        <th style={{textAlign:"start"}}>Date_Of_Birth</th>
+                        <th style={{textAlign:"start"}}>Branch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  {clients.length > 0 ? clients.map(client => {
+                    return (
+                      <tr key={client.id}>
+                        <td><a id={client.id} href='#' onClick={goToClientDetails}>{client.client_id}</a></td>
+                        <td>{client.fullname}</td>
+                        <td>{client.type_of_client}</td>
+                        <td>{client.phone_number}</td>
+                        <td>{client.gender}</td>
+                        {/* <td>{convertDate(client.registration_date)}</td>  
+                        <td>{convertDate(client.date_of_birth)} <em>({getAge(client.date_of_birth)} years)</em></td> */}
+                        <td>13/08/22</td>  
+                        <td>27/02/27<em>(27 years)</em></td>
+                        <td>{client.branch}</td>
+                      </tr>
+                    )
+                  }) : <tr><td colSpan={8} style={{textAlign: 'center'}}>No clients could be found.</td></tr>}
+                </tbody>
+            </table>
+        </div>
+        <Footer nextPageNumber={nextPageNumber} loadMoreClients={loadMoreClients} loadingMore={loadingMore}/>
+    </>
+  )
+}
+
+export default ClientsTable;
