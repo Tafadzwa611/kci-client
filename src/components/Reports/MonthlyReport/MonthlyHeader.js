231bf456d789bdb8e5ac0d030e1cc6c6e076fc2e
Tafadzwa Kuno <tkuno157@gmail.com>
2022-08-20 17:32:14 +0200

    updated monthly report

--- src/components/Reports/MonthlyReport/MonthlyHeader.js
+++ src/components/Reports/MonthlyReport/MonthlyHeader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MonthlyHeader = ({ changeOrder, order, disableSelect }) => {
     return (
-        <div className="header-container font-12">
-            <select onChange={changeOrder} value={order} disabled={disableSelect} >
+        <div className="header-container font-12" style={{padding:"0", border:"none"}}>
+            <select onChange={changeOrder} value={order} disabled={disableSelect} style={{margin:"0"}}>
                 <option value={'-id'}>Show newest first</option>
                 <option value={'id'}>Show oldest first</option>
             </select>
