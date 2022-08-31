d0b6b28fe6a6c052c4817d97711da424bab760ef
Tafadzwa Kuno <tkuno157@gmail.com>
2022-08-31 12:05:11 +0200

    all loans dashboard updatge

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { makeRequest } from '../../utils/utils';
 import LoansReleased from './LoansReleased/LoansReleased';
 import LoanCollections from './LoanCollections/LoanCollections';
 import AllLoans from './AllLoans/AllLoans';
+import AllClients from './AllClients/AllClients';
 
 export default function Dashboard() {
 
@@ -296,52 +297,7 @@ export default function Dashboard() {
               <div className="book-value-section">
 
                 <AllLoans branches={branches} currencies={currencies} div1={div1} showDiv1={showDiv1} />
-
-                  <div className="loans_clients_summary_container" style={{marginTop:"20px"}}>
-
-                      <div className="loans_clients_summary_container-header" onClick={showDiv2}>
-                          <div>
-                              <h1 className="dashboard-section-title">All Clients</h1>
-                              <span className="dashboard-section-amount-or-number">5789</span>
-                          </div>
-
-                          <i className={div2 ? 'uil uil-angle-down arrow_rotate loans_clients_summary-arrow' : 'uil uil-angle-down loans_clients_summary-arrow'}></i>
-                      </div>
-
-                      <div className={div2 ? 'loan_client_item_list grid showDiv' : 'loan_client_item_list grid'}>
-
-                          <div className="loans__clients__data">
-                              <div className="loan_client_summary__titles">
-                                  <h3 className="loan_client_item_name">Female Individual Clients</h3>
-                                  <span className="loan_client_item_number">789 ( 80% ) / ZWL 1 987 890.88</span>
-                              </div>
-                              <div className="loan_client_item__bar">
-                                  <span className="loan_client_item__percentage" style={{width:"80%"}}></span>
-                              </div>
-                          </div>
-
-                          <div className="loans__clients__data">
-                              <div className="loan_client_summary__titles">
-                                  <h3 className="loan_client_item_name">Male Individual Clients</h3>
-                                  <span className="loan_client_item_number">657 ( 60% ) / ZWL 1 278 987.88</span>
-                              </div>
-                              <div className="loan_client_item__bar">
-                                  <span className="loan_client_item__percentage" style={{width:"60%"}}></span>
-                              </div>
-                          </div>
-
-                          <div className="loans__clients__data">
-                              <div className="loan_client_summary__titles">
-                                  <h3 className="loan_client_item_name"> Company/Co-operative Clients</h3>
-                                  <span className="loan_client_item_number">878 ( 85% ) / ZWL 909 878.90</span>
-                              </div>
-                              <div className="loan_client_item__bar">
-                                  <span className="loan_client_item__percentage" style={{width:"85%"}}></span>
-                              </div>
-                          </div>
-
-                      </div>
-                  </div>
+                <AllClients branches={branches} div2={div2} showDiv2={showDiv2} />
 
               </div>
 
