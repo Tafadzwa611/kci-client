39416856d9d666e778512a83d29b0ee51edfd570
Tafadzwa Kuno <tkuno157@gmail.com>
2022-09-29 13:47:41 +0200

    spinnner updated

---
+++ src/components/Loader/MiniLoader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MiniLoader = () => {
+    return (
+        <div>
+            <div className="mini-spinner"></div>
+        </div>
+    )
+}
+
+export default MiniLoader;
--- src/components/Loader/loader.js
+++ src/components/Loader/loader.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <svg className="loader-svg-rotate">
-            <circle className="loader-svg" cx="20" cy="20" r="20"></circle>
-        </svg>
+        <div>
+            <div className="spinner"></div>
+        </div>
     )
 }
 
