19bca5730eba79917d94f316cef9cac74acb399c
Tafadzwa Kuno <tkuno157@gmail.com>
2022-11-02 17:47:15 +0200

    updated clients

--- src/components/Clients/ViewClients/ViewClients.js
+++ src/components/Clients/ViewClients/ViewClients.js
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import ClientsList from '../ClientsList/ClientsList'
 import AddClient from '../add_client/AddClient';
-import PendingApprovalList from '../PendingApprovalList/PendingApprovalList';
-import InactiveList from '../InactiveList/InactiveList';
 
 
 const ViewClients = () => {
@@ -21,19 +19,11 @@ const ViewClients = () => {
                     <div className="bloc-tabs">
                             <button className={tab === "clients" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("clients")}> All Clients </button>
                             <button className={tab === "addclient" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("addclient")}> Add Client </button>
-                            <button className={tab === "pendingapproval" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("pendingapproval")}> Pending Approval </button>
-                            <button className={tab === "inactive" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("inactive")}> Inactive </button>
-                            <button className={tab === "active" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("active")}> Active </button>
-                            <button className={tab === "rejected" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("rejected")}> Rejected </button>
-                            <button className={tab === "left" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("left")}> Left </button>
-                            <button className={tab === "blacklisted" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("blacklisted")}> Blacklisted </button>
                     </div>
                     <div className='tab-content font-12' style={{marginTop:"3rem"}}>
                         {{
                             'clients': <ClientsList setMainTab={setTab}/>,
                             'addclient': <AddClient setMainTab={setTab}/>,
-                            'pendingapproval': <PendingApprovalList setMainTab={setTab}/>,
-                            'inactive': <InactiveList setMainTab={setTab}/>,
                         }[tab]}
                     </div>
                 </>
