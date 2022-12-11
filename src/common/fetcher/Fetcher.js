0f95d0c6f445c5a870fb613811ab54e18739867a
Tafadzwa Kuno <tkuno157@gmail.com>
2022-12-11 17:03:09 +0200

    updated client id modals

--- src/common/fetcher/Fetcher.js
+++ src/common/fetcher/Fetcher.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NetworkError from './NetworkError';
 import useFetch from './useFetch';
+import MiniLoader from '../../components/Loader/MiniLoader';
 
 const Fetcher = ({urls, children}) => {
   const {data, error, loading} = useFetch(urls);
 
   if (loading) {
-    return <div className='bloc-tabs'>Loading Fetcher...</div>
+    return <div className='bloc-tabs'><MiniLoader /></div>
   }
 
   if (error != undefined) {
