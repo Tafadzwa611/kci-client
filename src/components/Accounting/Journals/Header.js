c7a7a73ebf5177f85818768bde535ca7dd9be690
Tafadzwa Kuno <tkuno157@gmail.com>
2023-02-07 15:17:34 +0200

    updated header for journals

--- src/components/Accounting/Journals/Header.js
+++ src/components/Accounting/Journals/Header.js
@@ -14,14 +14,14 @@ const Header = ({
         <div className="header-container font-12" style={{padding:"0", border:"none"}}> 
             {
                 !asStatement && 
-                    <select onChange={changeOrder} value={order} disabled={disableSelect}>
+                    <select onChange={changeOrder} value={order} disabled={disableSelect} style={{margin:"0"}}>
                         <option value={'-date_created'}>Show newest first</option>
                         <option value={'date_created'}>Show oldest first</option>
                     </select>
             }
-            <div>
-                Showing {numberOfJournalsLoaded} of {journalCount} journals.
-                {accountId != '' && <>View As Account Statement <input type='checkbox' value={asStatement} onChange={e => setAsStatement(curr => !curr)}/></>}
+            <div style={{margin:"0", display:"flex", columnGap:"10px"}}>
+                <div style={{margin:"0"}}>{accountId != '' && <>View As Account Statement <input type='checkbox' value={asStatement} onChange={e => setAsStatement(curr => !curr)}/></>}</div>
+                <div style={{margin:"0"}}>Showing {numberOfJournalsLoaded} of {journalCount} journals.</div>
             </div>
         </div>
     );
