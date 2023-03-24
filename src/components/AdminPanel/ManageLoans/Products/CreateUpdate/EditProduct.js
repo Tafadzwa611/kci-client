20a7c2bbe0b86d1660eaeb7974321a7992048869
Alpha-Lend <datafros2019@gmail.com>
2023-03-24 16:19:37 +0200

    loan product

--- src/components/AdminPanel/ManageLoans/Products/CreateUpdate/EditProduct.js
+++ src/components/AdminPanel/ManageLoans/Products/CreateUpdate/EditProduct.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ProductForm from './ProductForm';
 import {editLoanProductSchema} from './schema';
-import { removeNull } from '../../../../../utils/utils';
-import { put } from './post';
+import { removeNull,removeEmptyValues } from '../../../../../utils/utils';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 function EditProduct({productGrps, initialValues, setView, setSelectedPrdct}) {
   removeNull(initialValues);
 
   const onSubmit = async (values, actions) => {
-    put(values, actions.setErrors, setSelectedPrdct, setView);
+    try {
+      const data = removeEmptyValues(values);
+      const CONFIG = {headers: {'X-CSRFToken': Cookies.get('csrftoken'), 'Accept': 'application/json', 'Content-Type': 'application/json'}};
+      await axios.put(`/loansapi/edit_loan_product/${data.id}/`, data, CONFIG);
+      setSelectedPrdct(values);
+      setView('list');
+    } catch (error) {
+      if (error.message === "Network Error") {
+        actions.setErrors({responseStatus: "Network Error"});
+      } else if (error.response.status === 400) {
+        actions.setErrors({responseStatus: error.response.status, ...error.response.data});
+      } else {
+        actions.setErrors({responseStatus: error.response.status});
+      }
+    }
   }
 
   const back = () => setView('list');
