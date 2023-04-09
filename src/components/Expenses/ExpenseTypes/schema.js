2c57cd202e42687da48ea6f632a1dda2295312a7
Tafadzwa Kuno <tkuno157@gmail.com>
2023-04-09 21:30:56 +0200

    expenses create and filter update

--- src/components/Expenses/ExpenseTypes/schema.js
+++ src/components/Expenses/ExpenseTypes/schema.js
@@ -2,5 +2,7 @@ import * as yup from 'yup';
 
 export const addSchema = yup.object().shape({
   name: yup.string().required('Required'),
-  is_active: yup.boolean().required('Required'),
+  is_active: yup.boolean(),
+  date_of_account: yup.date().required('Required'),
+  currency_id: yup.number().integer().required('Required'),
 });
