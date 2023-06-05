b464dfd7180bf66458193332be2a74cfa6c06043
Tafadzwa Kuno <tkuno157@gmail.com>
2023-06-05 16:45:02 +0200

    group navigation added

--- src/components/Payments/ExcelPaymentsReport/ExcelPaymentsReport.js
+++ src/components/Payments/ExcelPaymentsReport/ExcelPaymentsReport.js
@@ -158,17 +158,48 @@ function PaymentsReport({report, reportId}) {
   const [params, setParams] = useState(null);
   const [payments, setPayments] = useState({count: 0, next_page_num: 0, payments: []});
 
+  const statusClasses = {
+    'In Queue': 'badge-info',
+    'Failed': 'badge-danger',
+    'In Progress': 'badge-primary',
+    'Completed': 'badge-success'
+  };
+
+  const state = {
+    border: '1px solid transparent',
+    display: 'inline-block',
+    padding: '5px 12px',
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '20px',
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+    borderRadius: '2em',
+    color: '#ffffff',
+  };
+
   return (
       <>
           <>
+              <div style={{marginBottom:'20px'}}>
+                <a className="btn btn-default" href={`/app/#/payments/viewpayments/excelpayments/`}>Back</a>
+                <div style={{display:"flex", alignItems:"center", columnGap:"10px", marginTop:"1.5rem"}}>
+                  <span>Excel Payments Report</span>
+                  <span className={statusClasses[report.status]} style={state}><b>{report.status}</b></span>
+                </div>
+              </div>
               <Filter setPayments={setPayments} setParams={setParams} reportId={reportId}/>
-              <ReportCard report={report}/>
-              <div style={{paddingTop: '2rem'}}></div>
-              <Payments
-                  payments={payments} 
-                  params={params}
-                  setPayments={setPayments}
-              />
+              {report.status === 'Completed' &&
+                <>
+                  <ReportCard report={report}/>
+                  <div style={{paddingTop: '2rem'}}></div>
+                  <Payments
+                      payments={payments} 
+                      params={params}
+                      setPayments={setPayments}
+                  />
+                </>
+              }
           </>
       </>
   )
