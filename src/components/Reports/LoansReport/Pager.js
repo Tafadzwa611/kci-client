f8b96688eabbf781b2ac17a2f6b88ae0d4d48d81
Tafadzwa Kuno <tkuno157@gmail.com>
2023-06-09 18:42:20 +0200

    loans report update

---
+++ src/components/Reports/LoansReport/Pager.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Pager = ({
+  prevPageNumber,
+  nextPageNumber,
+  setLoansReportData,
+  params
+}) => {
+  const [errors, setErrors] = useState(null);
+
+  const onClick = async (evt) => {
+    try {
+      const pageNum = evt.target.innerText === 'Next' ? nextPageNumber : prevPageNumber;
+      params.set('page_num', pageNum);
+      const response = await axios.get('/reportsapi/loans-report/', {params: params});
+      setLoansReportData(response.data);
+    } catch (error) {
+      if (error.message === 'Network Error') {
+        setErrors({detail: 'Network Error'});
+      } else {
+        setErrors({detail: 'Server Error'});
+      }
+    }
+  }
+
+  return (
+    <div className='footer-container font-12 text-light' style={{display:'flex', columnGap:'3px'}}>
+      {errors && JSON.stringify(errors)}
+      {prevPageNumber && <><button className='btn btn-default' onClick={onClick}>Back</button><br/></>}
+      {nextPageNumber ? <button className='btn btn-default' onClick={onClick}>Next</button>: null}
+    </div>
+  )
+}
+
+export default Pager;
