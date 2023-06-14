c94bd72f407f12b99a8b9f999b86396ae7c5e23d
Tafadzwa Kuno <tkuno157@gmail.com>
2023-06-14 16:53:38 +0200

    sub accounts update

--- src/components/Accounting/ChartsOfAccounts/AssetsSubAccounts/DateRange.js
+++ src/components/Accounting/ChartsOfAccounts/AssetsSubAccounts/DateRange.js
@@ -1,84 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
+import React from 'react';
+import { Form, Formik } from 'formik';
+import {
+  NonFieldErrors,
+} from '../../../../common';
+import {
+    CustomDatePickerFilter,
+    CustomSelectFilter,
+    CustomMultiSelectFilter,
+    SubmitButtonFilter
+} from '../../../../common';
+import { useBranches } from '../../../../contexts/BranchesContext';
+import { useCurrencies } from '../../../../contexts/CurrenciesContext';
+import axios from 'axios';
+import { removeEmptyValues } from '../../../../utils/utils';
 
-const DateRange = (props) => {
-    const {
-        onSubmit, 
-        setSubAccounts, 
-        changeCurrency,
-        branches,
-        setBranchIds,
-        currency,
-        currencyId,
-    } = props;
+const DateRange = ({setMainAccounts, setParams}) => {
+  const initialValues = {
+    branch_ids: [],
+    page_num: 1,
+    acc_type: '',
+  };
+  const {branches} = useBranches();
+  const {currencies} = useCurrencies();
 
-    const style = {
-        control: base => ({
-          ...base,
-          border: '1px solid #dee2e6',
-          boxShadow: "none",
-          '&:hover':'1px solid #dee2e6',
-        })
-    };
-
-    const [optionSelected, setOptionSelected] = useState([]);
-    const selectorBranches = [...branches.map(result => ({...result, label: result.name, value:result.id}))];
-  
-    const handleMultiSelect = selected => {
-      setOptionSelected(selected);
-      setBranchIds(selected.map(branch => branch.id));
+  const getParams = (values) => {
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(values)) {
+      if (Array.isArray(value)) {
+        value.forEach(el => params.append(key, el));
+      }else {
+        params.append(key, value);
+      }
     }
+    return params
+  }
 
-    console.log(currencyId)
-  
+  const onSubmit = async (values, actions) => {
+    try {
+      const data = removeEmptyValues(values);
+      const params = getParams(data);
+      setParams(params);
+      const response = await axios.get('/acc-api/main-accounts-list/', {params: params});
+      setMainAccounts(response.data);
+    } catch (error) {
+      if (error.message === "Network Error") {
+        actions.setErrors({responseStatus: "Network Error"});
+      } else if (error.response.status >= 400 && error.response.status < 500) {
+        actions.setErrors({responseStatus: error.response.status, ...error.response.data});
+      } else {
+        actions.setErrors({responseStatus: error.response.status});
+      }
+    }
+  }
 
-    return (
-
-        <div className="font-13 text-light">
-
-            <form onSubmit={onSubmit}>
-                <div className="view_search_container online__applications font-13" style={{border:"none", padding:"0", marginBottom:'1.5rem'}}>
-                    <div className="row-payments-container" style={{width:"72%"}}>
-                        <Select
-                            isMulti
-                            name='colors'
-                            options={[props.allOption, ...selectorBranches]}
-                            value={optionSelected}
-                            classNamePrefix='select'
-                            className='basic-multi-select'
-                            placeholder='Select Branches'
-                            onChange={selected => {
-                                if (selected !== null && selected.length > 0 && selected[selected.length - 1].value === props.allOption.value) {
-                                return handleMultiSelect(selectorBranches);
-                                }
-                                handleMultiSelect(selected);
-                            }}
-                            styles={style}
-                        />
-                    </div>
-                    <div className="row-payments-container" style={{width:"15%"}}>
-                        <select className='custom-select-form row-form' style={{margin:"0"}} value={currencyId} onChange={changeCurrency}>
-                            {currency.map(crncy => {
-                                return <option key={crncy.id} value={crncy.id}>{crncy.shortname}</option>
-                            })}
-                        </select>
-                    </div>
-                    <div style={{display:"flex", flexDirection:"column", width:"10%"}}>
-                        <button type="submit" className="btn btn-olive" >Apply_Filters_!</button>
-                    </div>
-                </div>
-            </form>
-
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({isSubmitting, setFieldValue, errors}) => (
+          <div className="search_background">
+            <div className="row-containers" style={{border:"none"}}>
+                <Form>
+                    <NonFieldErrors errors={errors}>
+                        <div className="row row-payments row-loans" style={{marginTop:"1rem"}}>
+                            <div className="row-payments-container" style={{width:"80%"}}>
+                                <CustomMultiSelectFilter
+                                    label='Branches'
+                                    name='branch_ids'
+                                    options={branches.map(br => ({label: br.name, value:br.id}))}
+                                    setFieldValue={setFieldValue}
+                                    required
+                                />
+                            </div>
+                            <div className="row-payments-container" style={{width:"10%"}}>
+                                <CustomSelectFilter label='Currency' name='currency_id' required>
+                                    <option value=''>------</option>
+                                    {currencies.map(currency => <option key={currency.id} value={currency.id}>{currency.fullname}</option>)}
+                                </CustomSelectFilter>
+                            </div>
+                            <SubmitButtonFilter isSubmitting={isSubmitting}/>
+                        </div>
+                    </NonFieldErrors>
+                </Form>
+            </div>
         </div>
-        
-    );
+      )}
+    </Formik>
+  );
 }
 
-DateRange.defaultProps = {
-    allOption: {
-        label: 'Select all',
-        value: '*'
-    }
-};
-
 export default DateRange;
+
--- src/components/Accounting/ChartsOfAccounts/ChartsOfAccounts.js
+++ src/components/Accounting/ChartsOfAccounts/ChartsOfAccounts.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import MainAccounts from './MainAccounts/MainAccounts';
 import SubAccounts from './SubAccounts/SubAccounts';
-import AssetsSubAccounts from './AssetsSubAccounts/AssetsSubAccounts';
-import LiabilitiesSubAccounts from './LiabilitiesSubAccounts/LiabilitiesSubAccounts';
-import EquitySubAccounts from './EquitySubAccounts/EquitySubAccounts';
-import IncomeSubAccounts from './IncomeSubAccounts/IncomeSubAccounts';
-import ExpensesSubAccounts from './ExpensesSubAccounts/ExpensesSubAccounts';
+// import AssetsSubAccounts from './AssetsSubAccounts/AssetsSubAccounts';
+// import LiabilitiesSubAccounts from './LiabilitiesSubAccounts/LiabilitiesSubAccounts';
+// import EquitySubAccounts from './EquitySubAccounts/EquitySubAccounts';
+// import IncomeSubAccounts from './IncomeSubAccounts/IncomeSubAccounts';
+// import ExpensesSubAccounts from './ExpensesSubAccounts/ExpensesSubAccounts';
 
 const ChartsOfAccounts = () => {
     const [tab, setTab] = useState('mainaccs');
@@ -14,21 +14,21 @@ const ChartsOfAccounts = () => {
             <div className="bloc-tabs">
                     <button className={tab === "mainaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("mainaccs")}> Main Accounts </button>
                     <button className={tab === "subaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("subaccs")}> Sub Accounts </button>
-                    <button className={tab === "assetssubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("assetssubaccs")}> Assets </button>
+                    {/* <button className={tab === "assetssubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("assetssubaccs")}> Assets </button>
                     <button className={tab === "liabilitiessubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("liabilitiessubaccs")}> Liabilities </button>
                     <button className={tab === "equitysubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("equitysubaccs")}> Equity </button>
                     <button className={tab === "incomesubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("incomesubaccs")}> Income </button>
-                    <button className={tab === "expensessubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("expensessubaccs")}> Expenses </button>
+                    <button className={tab === "expensessubaccs" ? "tabs-client active-tabs" : "tabs-client"} onClick={e=> setTab("expensessubaccs")}> Expenses </button> */}
             </div>
             <div className='tab-content font-12' style={{marginTop:"3rem"}}>
                 {{
                     'mainaccs': <MainAccounts setTab={setTab}/>,
                     'subaccs': <SubAccounts setTab={setTab}/>,
-                    'assetssubaccs': <AssetsSubAccounts setTab={setTab}/>,
-                    'liabilitiessubaccs': <LiabilitiesSubAccounts setTab={setTab}/>,
-                    'equitysubaccs': <EquitySubAccounts setTab={setTab}/>,
-                    'incomesubaccs': <IncomeSubAccounts setTab={setTab}/>,
-                    'expensessubaccs': <ExpensesSubAccounts setTab={setTab}/>,
+                    // 'assetssubaccs': <AssetsSubAccounts setTab={setTab}/>,
+                    // 'liabilitiessubaccs': <LiabilitiesSubAccounts setTab={setTab}/>,
+                    // 'equitysubaccs': <EquitySubAccounts setTab={setTab}/>,
+                    // 'incomesubaccs': <IncomeSubAccounts setTab={setTab}/>,
+                    // 'expensessubaccs': <ExpensesSubAccounts setTab={setTab}/>,
                 }[tab]}
             </div>
         </>
