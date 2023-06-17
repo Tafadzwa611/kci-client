4da39b7fada999039f8ab89611e227050054e50e
Tafadzwa Kuno <tkuno157@gmail.com>
2023-06-17 17:06:30 +0200

    daily report update

--- src/components/Reports/DailyReport/DailyReport.js
+++ src/components/Reports/DailyReport/DailyReport.js
@@ -1,102 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import Filter from './Filter'
-import Header from './Header';
+import React, {useState} from 'react';
+import Filter from './Filter';
 import Table from './Table';
-import Footer from './Footer';
-import NoData from '../ClientsReport/NoData';
-import { makeRequest } from '../../../utils/utils';
-import MiniLoader from '../../Loader/MiniLoader';
 
-const DailyReport = ({loggedInUser}) => {
-    const [dateOfReport, setDateOfReport] = useState('');
-    const [selectedBranchesIds, setSelectedBranchesIds] = useState([]);
-    const [report, setReport] = useState(null);
-    const [msg, setMsg] = useState('Select month and at least one branch, then click fetch to view report.');
-    const [currencies, setCurrencies] = useState(null);
-    const [currencyId, setCurrencyId] = useState(null);
-    const [currencyIso, setCurrencyIso] = useState(null);
-    const [selectedBranches, setSelectedBranches] = useState([]);
-  
-    useEffect(() => {
-        fetchCurrencies()
-    }, []);
-  
-    useEffect(() => {
-        setReport(null);
-    }, [currencyId, dateOfReport, selectedBranchesIds]);
-  
-    const onSubmit = async (evt) => {
-        evt.preventDefault();
-        const data = await getReport();
-        setCurrencyIso(currencies.filter(currency => currency.id == currencyId)[0].shortname);
-        setReport(data);
-    }
-  
-    async function getReport() {
-        const url = getUrl();
-        const response = await makeRequest.get(url, {timeout: 8000});
-        if (response.ok) {
-            const data = await response.json();
-            return data
-        } else {
-            const error = await response.json();
-            console.log(error);
-        }
-    }
-  
-    const getUrl = () => {
-        let url = `/reportsapi/daily-report/?currency_id=${currencyId}`;
-        if (dateOfReport !== '') {url += `&date_of_report=${dateOfReport}`};
-        selectedBranchesIds.forEach(id => (url += `&branch_ids=${id}`));
-        return url
-    }
-  
-    async function fetchCurrencies() {
-        try {
-            const response = await makeRequest.get('/usersapi/list_currencies/', {timeout: 8000});
-            if (response.ok) {
-                const data = await response.json();
-                if (currencyId===null) {
-                    const zwlId = data.filter(currency => currency.shortname === 'ZWL')[0].id;
-                    setCurrencyId(zwlId);
-                }
-                return setCurrencies([...data.map(result => ({...result, label: result.shortname, value:result.id}))]);
-            }else {
-                const error = await response.json();
-                console.log(error);
-            }
-        }catch(error) {
-            console.log(error);
-        }
-    }
-  
-    if (currencies===null) {
-        return <MiniLoader />
-    }
-  
+function DailyReport({loggedInUser}) {
+    const [params, setParams] = useState(null);
+    const [dailyData, setDailyData] = useState(null);
+    const [intValues, setIntValues] = useState([])
+
+
     return (
         <>
-            <Filter 
-                currencies={currencies}
-                currencyId={currencyId}
-                setCurrencyId={setCurrencyId}
-                dateOfReport={dateOfReport}
-                setDateOfReport={setDateOfReport}
-                disableFetch={selectedBranchesIds.length === 0}
-                onSubmit={onSubmit}
-                updateSelectedBranchesId={setSelectedBranchesIds}
-                setSelectedBranches={setSelectedBranches}
-            />
-            <Header dateOfReport={dateOfReport} />
-            {report != null ?
-                <>
-                    <Table report={report} currencyIso={currencyIso} dateOfReport={dateOfReport} date={dateOfReport} selectedBranches={selectedBranches} loggedInUser={loggedInUser}/>
-                    <Footer />
-                </> :
-                  <NoData msg={msg} />
+            <Filter setDailyData={setDailyData} setParams={setParams} setIntValues={setIntValues}/>
+            <div style={{paddingTop: '2rem'}}></div>
+            {dailyData &&
+                <Table
+                    report={dailyData} 
+                    loggedInUser={loggedInUser}
+                    intValues={intValues}
+                />
             }
         </>
-    );
+    )
 }
 
 export default DailyReport;
--- src/components/Reports/DailyReport/Filter.js
+++ src/components/Reports/DailyReport/Filter.js
@@ -1,109 +1,97 @@
-import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
-import { makeRequest } from '../../../utils/utils';
+import React from 'react';
+import { Form, Formik } from 'formik';
+import {
+  NonFieldErrors,
+} from '../../../common';
+import {
+    CustomDatePickerFilter,
+    CustomSelectFilter,
+    CustomMultiSelectFilter,
+    SubmitButtonFilter
+} from '../../../common';
+import { useCurrencies } from '../../../contexts/CurrenciesContext';
+import { useBranches } from '../../../contexts/BranchesContext';
+import axios from 'axios';
+import { removeEmptyValues } from '../../../utils/utils';
 
-const Filter = (props) => {
-    const [optionSelected, setOptionSelected] = useState([]);
-    const [branches, setBranches] = useState([]);
-    const {currencies, currencyId, setCurrencyId, dateOfReport, setDateOfReport, onSubmit,
-        disableFetch, updateSelectedBranchesId, setSelectedBranches} = props;
-    const fetchStyles = disableFetch ? {pointerEvents: 'none', opacity: '0.7'} : {};
-  
-    useEffect(() => {
-        fetchBranches();
-    }, []);
-  
-    const handleMultiSelect = selected => {
-        setOptionSelected(selected);
-        setSelectedBranches(selected);
-        updateSelectedBranchesId(selected.map(branch => branch.id));
-    }
-  
-    const changeCurrency = (evt) => {
-        setCurrencyId(evt.target.value);
-    }
-  
-    async function fetchBranches() {
-        try {
-            const response = await makeRequest.get('/usersapi/get-branches/', {timeout: 8000});
-            if (response.ok) {
-                const data = await response.json();
-                return setBranches([...data.results.map(result => ({...result, label: result.name, value:result.id}))]);
-            }else {
-                const error = await response.json();
-                console.log(error);
-            }
-        }catch(error) {
-            console.log(error);
-        }
-    }
+const Filter = ({setDailyData, setParams, setIntValues}) => {
+  const initialValues = {
+    branch_ids: [],
+    page_num: 1,
+    date_of_report: '',
+  };
+  const {currencies} = useCurrencies();
+  const {branches} = useBranches();
 
-    const style = {
-        control: base => ({
-            ...base,
-            border: '1px solid #dee2e6',
-            boxShadow: "none",
-            '&:hover':'1px solid #dee2e6',
-        })
-    };
+  const getParams = (values) => {
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(values)) {
+      if (Array.isArray(value)) {
+        value.forEach(el => params.append(key, el));
+      }else {
+        params.append(key, value);
+      }
+    }
+    return params
+  }
 
-    return (
-        <form onSubmit={onSubmit}>
-            <div className="view_search_container online__applications font-13" style={{border:"none", padding:"0"}}>
-                <div className="row-payments-container" style={{width:"48%"}}>
-                    <label className="form-label row-label">Enter Date</label>
-                    <div className="input-group" style={{margin:"10px 0 0"}}>
-                        <i className="uil uil-calendar-alt"></i>
-                        <input 
-                            type='date' 
-                            value={dateOfReport} 
-                            onKeyDown={(e) => e.preventDefault()} 
-                            onChange={(e) => setDateOfReport(e.target.value)} 
-                            className='custom-select-form row-form input-background'
-                        />
-                    </div>
-                </div>
-                <div className="row-payments-container" style={{width:"48%"}}>
-                    <label className="form-label row-label">Select Currency</label>
-                    <select className='custom-select-form row-form' value={currencyId} onChange={changeCurrency} style={{margin:"10px 0 0"}}>
-                        {currencies.map(currency => {
-                            return <option key={currency.id} value={currency.id}>{currency.shortname}</option>
-                        })}
-                    </select>
-                </div>
+  const onSubmit = async (values, actions) => {
+    try {
+      const data = removeEmptyValues(values);
+      const params = getParams(data);
+      setParams(params);
+      setIntValues(values);
+      const response = await axios.get('/reportsapi/daily-report/', {params: params});
+      setDailyData(response.data);
+    } catch (error) {
+      if (error.message === "Network Error") {
+        actions.setErrors({responseStatus: "Network Error"});
+      } else if (error.response.status >= 400 && error.response.status < 500) {
+        actions.setErrors({responseStatus: error.response.status, ...error.response.data});
+      } else {
+        actions.setErrors({responseStatus: error.response.status});
+      }
+    }
+  }
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({isSubmitting, setFieldValue, errors}) => (
+          <div className="search_background">
+            <div className="row-containers" style={{border:"none"}}>
+                <Form>
+                    <NonFieldErrors errors={errors}>
+                        <div className="row row-payments row-loans" style={{marginTop:"1rem"}}>
+                            <div className="row-payments-container" style={{width:"49%"}}>
+                                <CustomDatePickerFilter label='Date' name='date_of_report' setFieldValue={setFieldValue} required/>
+                            </div>
+                            <div className="row-payments-container" style={{width:"49%"}}>
+                                <CustomSelectFilter label='Currency' name='currency_id' required>
+                                    <option value=''>------</option>
+                                    {currencies.map(currency => <option key={currency.id} value={currency.id}>{currency.fullname}</option>)}
+                                </CustomSelectFilter>
+                            </div>
+                        </div>
+                        <div style={{marginTop:"1rem", display:"flex", justifyContent:"space-between"}}>
+                            <div style={{width:"90%"}}>
+                                <CustomMultiSelectFilter
+                                    label='Branches'
+                                    name='branch_ids'
+                                    options={branches.map(br => ({label: br.name, value:br.id}))}
+                                    setFieldValue={setFieldValue}
+                                    required
+                                />
+                            </div>
+                            <SubmitButtonFilter isSubmitting={isSubmitting}/>
+                        </div>
+                    </NonFieldErrors>
+                </Form>
             </div>
-            <div className="view_search_container online__applications font-13" style={{border:"none", padding:"0", marginTop:"1rem"}}>
-                <div className="row-payments-container" style={{width:"85%"}}>
-                    <Select
-                        isMulti
-                        name='colors'
-                        options={[props.allOption, ...branches]}
-                        value={optionSelected}
-                        classNamePrefix='select'
-                        className='basic-multi-select'
-                        placeholder='Select Branches'
-                        onChange={selected => {
-                            if (selected !== null && selected.length > 0 && selected[selected.length - 1].value === props.allOption.value) {
-                            return handleMultiSelect(branches);
-                            }
-                            handleMultiSelect(selected);
-                        }}
-                        styles={style}
-                    />
-                </div>
-                <div style={{display:"flex", flexDirection:"column"}}>
-                    <button type="submit" className="btn btn-olive" style={fetchStyles} disabled={disableFetch}>Apply_filters_!</button>
-                </div>
-            </div>
-        </form>
-    )
+        </div>
+      )}
+    </Formik>
+  );
 }
 
-Filter.defaultProps = {
-    allOption: {
-        label: 'Select all',
-        value: '*'
-    }
-};
-
 export default Filter;
+
