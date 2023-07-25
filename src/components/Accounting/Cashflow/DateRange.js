0197c77341fc405a7744cbde07e0dd838e4e5937
Alpha-Lend <datafros2019@gmail.com>
2023-07-25 09:58:04 +0200

    cashflow

--- src/components/Accounting/Cashflow/DateRange.js
+++ src/components/Accounting/Cashflow/DateRange.js
@@ -2,50 +2,28 @@ import React from 'react';
 import { Form, Formik } from 'formik';
 import {
   NonFieldErrors,
-} from '../../../common';
-import {
-    CustomDatePickerFilter,
-    CustomSelectFilter,
-    CustomMultiSelectFilter,
-    SubmitButtonFilter
+  CustomDatePickerFilter,
+  CustomSelectFilter,
+  CustomSelectRemoteFilter,
+  SubmitButtonFilter
 } from '../../../common';
 import { useCurrencies } from '../../../contexts/CurrenciesContext';
-import { useBranches } from '../../../contexts/BranchesContext';
 import axios from 'axios';
-import { removeEmptyValues } from '../../../utils/utils';
+import { getParams } from '../../../utils/utils';
 
-const DateRange = ({setCashFlowData, setParams}) => {
-  const initialValues = {
-    branch_ids: [],
-    page_num: 1,
-    min_date: '',
-    max_date: '',
-  };
+const DateRange = ({setCashFlowData}) => {
+  const initialValues = {cash_account: '', min_date: '', max_date: ''};
   const {currencies} = useCurrencies();
-  const {branches} = useBranches();
-
-  const getParams = (values) => {
-    const params = new URLSearchParams();
-    for (const [key, value] of Object.entries(values)) {
-      if (Array.isArray(value)) {
-        value.forEach(el => params.append(key, el));
-      }else {
-        params.append(key, value);
-      }
-    }
-    return params
-  }
 
   const onSubmit = async (values, actions) => {
     try {
-      const data = removeEmptyValues(values);
+      const data = {min_date: values.min_date, max_date: values.max_date, cash_account_id: values.cash_account.id};
       const params = getParams(data);
-      setParams(params);
       const response = await axios.get('/acc-api/cash-flow-statement/', {params: params});
       setCashFlowData(response.data);
     } catch (error) {
-      if (error.message === "Network Error") {
-        actions.setErrors({responseStatus: "Network Error"});
+      if (error.message === 'Network Error') {
+        actions.setErrors({responseStatus: 'Network Error'});
       } else if (error.response.status >= 400 && error.response.status < 500) {
         actions.setErrors({responseStatus: error.response.status, ...error.response.data});
       } else {
@@ -56,40 +34,45 @@ const DateRange = ({setCashFlowData, setParams}) => {
 
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {({isSubmitting, setFieldValue, errors}) => (
-          <div className="search_background">
-            <div className="row-containers" style={{border:"none"}}>
-                <Form>
-                    <NonFieldErrors errors={errors}>
-                        <div className="row row-payments row-loans" style={{marginTop:"1rem"}}>
-                            <div className="row-payments-container" style={{width:"32%"}}>
-                                <CustomDatePickerFilter label='Min Date' name='min_date' setFieldValue={setFieldValue} required/>
-                            </div>
-                            <div className="row-payments-container" style={{width:"32%"}}>
-                                <CustomDatePickerFilter label='Max Date' name='max_date' setFieldValue={setFieldValue} required/>
-                            </div>
-                            <div className="row-payments-container" style={{width:"32%"}}>
-                                <CustomSelectFilter label='Currency' name='currency_id' required>
-                                    <option value=''>------</option>
-                                    {currencies.map(currency => <option key={currency.id} value={currency.id}>{currency.fullname}</option>)}
-                                </CustomSelectFilter>
-                            </div>
-                        </div>
-                        <div style={{marginTop:"1rem", display:"flex", justifyContent:"space-between"}}>
-                            <div style={{width:"90%"}}>
-                                <CustomMultiSelectFilter
-                                    label='Branches'
-                                    name='branch_ids'
-                                    options={branches.map(br => ({label: br.name, value:br.id}))}
-                                    setFieldValue={setFieldValue}
-                                    required
-                                />
-                            </div>
-                            <SubmitButtonFilter isSubmitting={isSubmitting}/>
-                        </div>
-                    </NonFieldErrors>
-                </Form>
-            </div>
+      {({isSubmitting, setFieldValue, errors, values}) => (
+        <div className='search_background'>
+          <div className='row-containers' style={{border:'none'}}>
+            <Form>
+              <NonFieldErrors errors={errors}>
+                <div className='row row-payments row-loans' style={{marginTop:'1rem'}}>
+                  <div className='row-payments-container' style={{width:'32%'}}>
+                    <CustomDatePickerFilter label='Min Date' name='min_date' setFieldValue={setFieldValue} required/>
+                  </div>
+                  <div className='row-payments-container' style={{width:'32%'}}>
+                    <CustomDatePickerFilter label='Max Date' name='max_date' setFieldValue={setFieldValue} required/>
+                  </div>
+                  <div className='row-payments-container' style={{width:'32%'}}>
+                    <CustomSelectFilter label='Currency' name='currency_id' required>
+                      <option value=''>------</option>
+                      {currencies.map(currency => <option key={currency.id} value={currency.id}>{currency.fullname}</option>)}
+                    </CustomSelectFilter>
+                  </div>
+                </div>
+                <div style={{marginTop:'1rem', display:'flex', justifyContent:'space-between'}}>
+                  {values.currency_id ? 
+                  <div className='row-payments-container' style={{width:'90%'}}>
+                    <CustomSelectRemoteFilter
+                      label='Account'
+                      url='/acc-api/search_account/'
+                      selected={values.account}
+                      params={[{key: 'currency_id', value: values.currency_id}]}
+                      setFieldValue={setFieldValue}
+                      queryParamName='query'
+                      placeholder='Search Account'
+                      name='cash_account'
+                      required
+                    />
+                  </div> : null}
+                  <SubmitButtonFilter isSubmitting={isSubmitting}/>
+                </div>
+              </NonFieldErrors>
+            </Form>
+          </div>
         </div>
       )}
     </Formik>
