7b8ebc4911d16a71476fff0fc8a0658f5137c476
Alpha-Lend <datafros2019@gmail.com>
2023-08-05 18:11:18 +0200

    daily report

--- src/components/Reports/DailyReport/DailyReport.js
+++ src/components/Reports/DailyReport/DailyReport.js
@@ -2,25 +2,16 @@ import React, {useState} from 'react';
 import Filter from './Filter';
 import Table from './Table';
 
-function DailyReport({loggedInUser}) {
-    const [params, setParams] = useState(null);
-    const [dailyData, setDailyData] = useState(null);
-    const [intValues, setIntValues] = useState([])
+function DailyReport() {
+  const [report, setReport] = useState(null);
 
-
-    return (
-        <>
-            <Filter setDailyData={setDailyData} setParams={setParams} setIntValues={setIntValues}/>
-            <div style={{paddingTop: '2rem'}}></div>
-            {dailyData &&
-                <Table
-                    report={dailyData} 
-                    loggedInUser={loggedInUser}
-                    intValues={intValues}
-                />
-            }
-        </>
-    )
+  return (
+    <>
+      <Filter setReport={setReport}/>
+      <div style={{paddingTop: '2rem'}}></div>
+      {report ? <Table report={report}/> : null}
+    </>
+  )
 }
 
 export default DailyReport;
