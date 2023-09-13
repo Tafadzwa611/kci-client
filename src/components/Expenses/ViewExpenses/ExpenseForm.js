aec25aebef0e6f92bed6041a8a493a4ee0eaaf9d
Alpha-Lend <datafros2019@gmail.com>
2023-09-13 15:04:16 +0200

    fix bug

--- src/components/Expenses/ViewExpenses/ExpenseForm.js
+++ src/components/Expenses/ViewExpenses/ExpenseForm.js
@@ -9,53 +9,47 @@ import {
 } from '../../../common';
 
 function ExpenseForm({expensetypes, fundaccounts, initialValues, validationSchema, onSubmit}) {
-    const [currencyId, seCurrencyId] = useState('');
-    const onChange = (setFieldValue, newValue) => {
-        setFieldValue('expense_type_id', newValue);
-        const exp_type = expensetypes.find(et => et.id == newValue);
-        seCurrencyId(exp_type.currency_id)
-    }
-    const newFundAccountArray = fundaccounts.filter(fa => fa.currency_id ==  currencyId);
+  const [currencyId, seCurrencyId] = useState('');
+  const onChange = (setFieldValue, newValue) => {
+    setFieldValue('expense_type_id', newValue);
+    const exp_type = expensetypes.find(et => et.id == newValue);
+    seCurrencyId(exp_type.currency_id);
+  }
+  const newFundAccountArray = fundaccounts.filter(fa => fa.currency_id ==  currencyId);
 
-    console.log(newFundAccountArray)
-
-    return (
-        <>
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-                {({ isSubmitting, errors, setFieldValue, values }) => (
-                <Form>
-                    <NonFieldErrors errors={errors}>
-                        <div className='divider divider-info'>
-                            <span>Expense Information</span>
-                        </div>
-                        <CustomSelect 
-                            label='Expense Type' 
-                            name='expense_type_id'  
-                            onChange={evt => onChange(setFieldValue, evt.target.value)}
-                            required
-                        >
-                            <option value=''>------</option>
-                            {expensetypes.filter(type => type.is_active).map(type => <option key={type.id} value={type.id}>{type.name}</option>)}
-                        </CustomSelect>
-                        <CustomSelect label='Fund Account' name='fund_account_id' required>
-                            <option value=''>------</option>
-                            {newFundAccountArray.map(account => <option key={account.id} value={account.id}>{account.general_ledger_name}</option>)}
-                        </CustomSelect>
-                        <CustomInput label='Expense Name' name='expense_name' type='text' required/>
-                        <CustomInput label='Expense Amount' name='expense_amount' type='number' required/>
-                        <CustomDatePicker label='Expense Date' name='expense_date' setFieldValue={setFieldValue} required/>
-                        <CustomInput label='Reference' name='reference' type='text' />
-                        <CustomInput label='Description' name='description' type='text' />
-                        <div className='divider divider-default' style={{padding: '1.25rem'}}></div>
-                        <div style={{display:'flex', justifyContent: 'flex-end'}}> 
-                        <SubmitButton isSubmitting={isSubmitting}/>
-                        </div>
-                    </NonFieldErrors>
-                </Form>
-                )}
-            </Formik>
-        </>
-    )
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      {({ isSubmitting, errors, setFieldValue }) => (
+        <Form>
+          <NonFieldErrors errors={errors}>
+            <div className='divider divider-info'><span>Expense Information</span></div>
+            <CustomSelect 
+              label='Expense Type' 
+              name='expense_type_id'  
+              onChange={evt => onChange(setFieldValue, evt.target.value)}
+              required
+            >
+              <option value=''>------</option>
+              {expensetypes.filter(type => type.is_active).map(type => <option key={type.id} value={type.id}>{type.currency__shortname} {type.name} {type.branch__name}</option>)}
+            </CustomSelect>
+            <CustomSelect label='Fund Account' name='fund_account_id' required>
+              <option value=''>------</option>
+              {newFundAccountArray.map(account => <option key={account.id} value={account.id}>{account.currency} {account.general_ledger_name} {account.branch}</option>)}
+            </CustomSelect>
+            <CustomInput label='Expense Name' name='expense_name' type='text' required/>
+            <CustomInput label='Expense Amount' name='expense_amount' type='number' required/>
+            <CustomDatePicker label='Expense Date' name='expense_date' setFieldValue={setFieldValue} required/>
+            <CustomInput label='Reference' name='reference' type='text' />
+            <CustomInput label='Description' name='description' type='text' />
+            <div className='divider divider-default' style={{padding: '1.25rem'}}></div>
+            <div style={{display:'flex', justifyContent: 'flex-end'}}>
+              <SubmitButton isSubmitting={isSubmitting}/>
+            </div>
+          </NonFieldErrors>
+        </Form>
+      )}
+    </Formik>
+  )
 }
 
 export default ExpenseForm;
