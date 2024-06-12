4ebb95eb98ff90eabd264ba8eb0c8145620a8007
Tafadzwa Kuno <tafadzwakuno@gmail.com>
2024-06-12 18:33:09 +0200

    home update

--- src/components/Home/UserStream.js
+++ src/components/Home/UserStream.js
@@ -5,155 +5,42 @@ function UserStream({open, setOpen}) {
  
     return (
         <Modal open={open} setOpen={setOpen} title='User Stream'>
-            <div>
-                <span>Tafadzwa Kuno Stream</span>
-            </div>
             <div style={{overflowX: 'auto'}}>
                 <div style={{height:'100vh'}}>
                     <div style={{position:'sticky', top:0}}>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                         <div className='timeline__section'>
                             <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
-                            </span>
-                        </div>
-                        <div className='timeline__section'>
-                            <span>
-                            <span className='link' style={{cursor:'pointer'}} onClick={(e) => setOpen(curr => !curr)}>Tafadzwa Kuno</span> logged in at 10:45am.
+                            <span style={{cursor:'pointer'}}>Tafadzwa Kuno</span> logged in at 10:45am.
                             </span>
                         </div>
                     </div>
