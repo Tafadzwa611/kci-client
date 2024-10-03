9ad56d4fe1f896d6ebe8870cfc8aa5c4b47d975c
Tafadzwa Kuno <tafadzwakuno@gmail.com>
2024-10-03 18:21:55 +0200

    update par tile

--- src/components/Reports/PortfolioAtRiskReport/Tile.js
+++ src/components/Reports/PortfolioAtRiskReport/Tile.js
@@ -41,7 +41,9 @@ function Tile({ par }) {
             <span>{par.currency} PAR {par.par_name}</span>
             <span>Number of loans {par.par_name} days late: <b>{par.loans_in_arrears_count}</b></span>
             <span>Total Loan Count: <b>{par.total_loan_count}</b></span>
-            <span>Principal At Risk: <b>{par.currency} {par.principal_at_risk}</b></span>
+            <span>
+              Principal At Risk: <b>{par.currency} {par.principal_at_risk}</b>  <em>(Interest Balance - {par.currency} {par.interest_at_risk},  Fees Balance - {par.currency} {par.fees_at_risk})</em>
+            </span>
             <span>Total Loan Portfolio: <b>{par.currency} {par.total_loan_portfolio}</b></span>
           </div>
         </div>
