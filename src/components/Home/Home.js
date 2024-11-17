5013229f0706bf9ba3667e0c1f9bf38b778d53e8
Alpha-Lend <datafros2019@gmail.com>
2024-11-17 16:16:42 +0200

    update activity log

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,44 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { Fetcher } from '../../common';
 
 function Home() {
-  const [logs, setLogs] = useState([]);
-
   useEffect(() => {
     document.title = 'Home';
-  }, []);
-
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const response = await axios.get("/reportsapi/audit-trail/?page_num=1&page_size=100");
-        setLogs(response.data.entries);
-      } catch(err) {
-        console.log(err);
-      }
-    }
-    fetch();
   }, []);
 
   return (
     <div style={{width:'50%'}}>
       <div style={{width:'100%'}} className='book-value-section card'>
-        <p>Activity Stream</p>
-        <div style={{overflowX: 'auto'}}>
-          <div style={{maxHeight:'150vh'}}>
-            <div style={{maxHeight:'145vh'}}>
-              <div style={{position:'sticky', top:0}}>
-                {logs.map(log => (
-                <div className='timeline__section' key={log.id}>
-                  <span>
-                    <span>Action: {log.action_name || log.action_type} | Entity: {log.entity} | Performed At: {log.event_timestamp} | Performed By: {log.actor_name}</span>
-                  </span>
+        <p>Activity Log</p>
+        <Fetcher urls={["/reportsapi/audit-trail/?page_num=1&page_size=100&latest_only=1"]}>
+          {({data}) => ( 
+            <div style={{overflowX: 'auto'}}>
+              <div style={{maxHeight:'150vh'}}>
+                <div style={{maxHeight:'145vh'}}>
+                  <div style={{position:'sticky', top:0}}>
+                    {data[0].entries.map(log => (
+                      <div className='timeline__section' key={log.id}>
+                        <span>
+                          <span>Action: {log.action_name || log.action_type} | Entity: {log.entity} | Performed At: {log.event_timestamp} | Performed By: {log.actor_name}</span>
+                        </span>
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                ))}
               </div>
             </div>
-          </div>
-        </div>
+          )}
+        </Fetcher>
       </div>
     </div>
   )
