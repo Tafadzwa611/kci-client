26c7fb4c4881e886f2afe9b2f89efca807b58b0a
Tafadzwa Kuno <tafadzwakuno@gmail.com>
2024-11-17 18:01:36 +0200

    home update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,30 +7,37 @@ function Home() {
   }, []);
 
   return (
-    <div style={{width:'50%'}}>
-      <div style={{width:'100%'}} className='book-value-section card'>
-        <p>Activity Log</p>
-        <Fetcher urls={["/reportsapi/audit-trail/?page_num=1&page_size=100&latest_only=1"]}>
-          {({data}) => ( 
-            <div style={{overflowX: 'auto'}}>
-              <div style={{maxHeight:'150vh'}}>
-                <div style={{maxHeight:'145vh'}}>
-                  <div style={{position:'sticky', top:0}}>
-                    {data[0].entries.map(log => (
-                      <div className='timeline__section' key={log.id}>
-                        <span>
-                          <span>Action: {log.action_name || log.action_type} | Entity: {log.entity} | Performed At: {log.event_timestamp} | Performed By: {log.actor_name}</span>
-                        </span>
-                      </div>
-                    ))}
+    <>
+      <div className='card'>
+        <div className='info-box'>
+          <span>You can use topbar for navigation.</span>
+        </div>
+      </div>
+      <div style={{width:'50%'}}>
+        <div style={{width:'100%', marginTop:'1.5rem'}} className='book-value-section card'>
+          <p>Activity Log</p>
+          <Fetcher urls={["/reportsapi/audit-trail/?page_num=1&page_size=100&latest_only=1"]}>
+            {({data}) => ( 
+              <div style={{overflowX: 'auto'}}>
+                <div style={{maxHeight:'150vh'}}>
+                  <div style={{maxHeight:'145vh'}}>
+                    <div style={{position:'sticky', top:0}}>
+                      {data[0].entries.map(log => (
+                        <div className='timeline__section' key={log.id}>
+                          <span>
+                            <span>Action: {log.action_name || log.action_type} | Entity: {log.entity} | Performed At: {log.event_timestamp} | Performed By: {log.actor_name}</span>
+                          </span>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
-        </Fetcher>
+            )}
+          </Fetcher>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
