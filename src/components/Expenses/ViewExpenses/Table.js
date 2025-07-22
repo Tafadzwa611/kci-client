91fe5f5cb240bbeea49a5fd4c89d355bff40c6c8
Tanaka <marambatanaka@gmail.com>
2025-07-22 19:15:20 +0200

    feat: enhance expense management components with new filters and data fetching

--- src/components/Expenses/ViewExpenses/Table.js
+++ src/components/Expenses/ViewExpenses/Table.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MainTable from './MainTable';
 import MiniTable from './MiniTable';
 import MiniExpenseDetails from './MiniExpenseDetails';
