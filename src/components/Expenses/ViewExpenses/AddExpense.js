8a406e932a76df065ccbd5ec18171c7517340cf3
Tanaka <marambatanaka@gmail.com>
2026-01-20 20:54:25 +0200

    expenses update

--- src/components/Expenses/ViewExpenses/AddExpense.js
+++ src/components/Expenses/ViewExpenses/AddExpense.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import {addSchema} from './schema';
-import ExpenseForm from './ExpenseForm';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { removeEmptyValues } from '../../../utils/utils';
+import { Form, Formik } from 'formik';
+import {
+  CustomMultiSelect,
+  CustomInput,
+  NonFieldErrors,
+  SubmitButton,
+  CustomDatePicker
+} from '../../../common';
+
 
 function AddExpense({expensetypes, fundaccounts}) {
+  const [currencyId, seCurrencyId] = React.useState('');
+  const [settings, setSettings] = React.useState(null);
   const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchSettings() {
+      try {
+        const response = await axios.get('/expensesapi/expense_settings/');
+        setSettings(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchSettings();
+  }, []);
+
+  if (!settings) {
+    return <div>Loading...</div>
+  }
 
   const initialValues = {
     expense_type_id: '',
@@ -37,13 +63,54 @@ function AddExpense({expensetypes, fundaccounts}) {
   }
 
   return (
-    <ExpenseForm
-      expensetypes={expensetypes}
-      fundaccounts={fundaccounts}
-      initialValues={initialValues}
-      validationSchema={addSchema}
-      onSubmit={onSubmit}
-    />
+    <Formik initialValues={initialValues} validationSchema={addSchema} onSubmit={onSubmit}>
+      {({ isSubmitting, errors, setFieldValue }) => (
+        <Form>
+          <NonFieldErrors errors={errors}>
+            <div className='divider divider-info'><span>Expense Information</span></div>
+            <CustomMultiSelect
+              label='Expense Type'
+              name='expense_type'
+              isMulti={false}
+              setFieldValue={(field_name, selectedOpts) => {
+                setFieldValue(field_name, selectedOpts);
+                setFieldValue('expense_type_id', selectedOpts.value);
+                const exp_type = expensetypes.find(et => et.id == selectedOpts.value);
+                seCurrencyId(exp_type.currency_id);
+              }}
+              options={expensetypes.filter(type => type.is_active).map(type => (
+                {label: `${type.currency__shortname} ${type.name} ${type.branch__name}`, value: type.id}
+              ))}
+              required
+            />
+            {settings.accounting_method === 1 && (
+              <CustomMultiSelect
+                label='Fund Account'
+                name='fund_account'
+                isMulti={false}
+                setFieldValue={(field_name, selectedOpts) => {
+                setFieldValue(field_name, selectedOpts);
+                setFieldValue('fund_account_id', selectedOpts.value);
+                }}
+                options={fundaccounts.filter(fa => fa.currency_id == currencyId).map(account => (
+                {label: `${account.currency} ${account.general_ledger_name} ${account.branch}`, value: account.id}
+                ))}
+                required
+              />
+            )}
+            <CustomInput label='Expense Name' name='expense_name' type='text' required/>
+            <CustomInput label='Expense Amount' name='expense_amount' type='number' required/>
+            <CustomDatePicker label='Expense Date' name='expense_date' setFieldValue={setFieldValue} required/>
+            <CustomInput label='Reference' name='reference' type='text' />
+            <CustomInput label='Description' name='description' type='text' />
+            <div className='divider divider-default' style={{padding: '1.25rem'}}></div>
+            <div style={{display:'flex', justifyContent: 'flex-end'}}>
+              <SubmitButton isSubmitting={isSubmitting}/>
+            </div>
+          </NonFieldErrors>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
--- src/components/Expenses/ViewExpenses/schema.js
+++ src/components/Expenses/ViewExpenses/schema.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const addSchema = yup.object().shape({
     expense_name: yup.string().required('Required'),
     expense_type_id: yup.number().integer().required('Required'),
-    fund_account_id: yup.number().integer().required('Required'),
+    fund_account_id: yup.number().integer(),
     expense_amount: yup.number().required('Required'),
     expense_date: yup.string().required('Required'),
     reference: yup.string(),
