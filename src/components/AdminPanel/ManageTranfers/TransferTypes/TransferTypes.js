668b5eea45639888d1d1a8dfc186ef9784a2b176
Tafadzwa Kuno <tafadzwakuno@gmail.com>
2026-02-01 22:29:22 +0200

    transfers added

--- src/components/AdminPanel/ManageTranfers/TransferTypes/TransferTypes.js
+++ src/components/AdminPanel/ManageTranfers/TransferTypes/TransferTypes.js
@@ -1,54 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Fetcher } from '../../../../common';
+import React from "react";
+import { Fetcher } from "../../../../common";
 
 function TransferTypes() {
+  const formatAccount = (a) => {
+    if (a == null) return "";
+    if (typeof a === "number" || typeof a === "string") return String(a);
+
+    const base = a.label ?? `${a.general_ledger_name ?? ""} - ${a.general_ledger_code ?? ""}`.trim();
+    const branch = a.branch_name ?? a.branch?.name ?? a.branch; // supports string/object/id
+    return branch ? `${base} (${branch})` : base;
+  };
+
   return (
-    <Fetcher urls={['/acc-api/transfer-types/']}>
-      {({data}) => (
-        <>
-          <div style={{padding:'0', border:'none', marginTop:'20px'}} className='table-container full__width font-12'>
-            <div className='full__table'>
-              <div className='table-responsive'>
-                <div className='table__height'>
-                  <table className='table'>
+    <Fetcher urls={["/acc-api/transfer-types/"]}>
+      {({ data }) => {
+        const transferTypes = data?.tranfertypes ?? data?.[0] ?? [];
+
+        return (
+          <div style={{ padding: "0", border: "none", marginTop: "20px" }} className="table-container full__width font-12">
+            <div className="full__table">
+              <div className="table-responsive">
+                <div className="table__height">
+                  <table className="table">
                     <tbody>
-                      <tr className='journal-details header' style={{position:'sticky', top: 0}}>
-                        <th>Transfer Type Name</th>
-                        <th>Receiving Accounts</th>
-                        <th>Sending Accounts</th>
+                      <tr className="journal-details header" style={{ position: "sticky", top: 0 }}>
+                        <th>Transfer_Type_Name</th>
+                        <th>Receiving_Accounts</th>
+                        <th>Sending_Accounts</th>
                       </tr>
-                      {data[0].map(transfer_type => {
-                        return (
-                          <tr key={transfer_type.id}>
-                            <td>
-                              <Link to={`/users/admin/managetransfers/createtransfertype/${transfer_type.id}`}>
-                                {transfer_type.name}
-                              </Link>
-                            </td>
-                            <td>
-                              {(transfer_type.receiving_accounts || [])
-                                .map(a => a.label || `${a.general_ledger_name} - ${a.general_ledger_code}`)
-                                .join(", ")}
-                            </td>
-                                                        <td>
-                              {(transfer_type.sending_accounts || [])
-                                .map(a => a.label || `${a.general_ledger_name} - ${a.general_ledger_code}`)
-                                .join(", ")}
-                            </td>
-                          </tr>
-                        )
-                      })}
+
+                      {transferTypes.map((transfer_type) => (
+                        <tr key={transfer_type.id}>
+                          <td>
+                              {transfer_type.name}
+                          </td>
+
+                          <td>
+                            {(transfer_type.receiving_accounts || []).map(formatAccount).filter(Boolean).join(", ")}
+                          </td>
+
+                          <td>
+                            {(transfer_type.sending_accounts || []).map(formatAccount).filter(Boolean).join(", ")}
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 </div>
-                </div>
+              </div>
             </div>
           </div>
-        </>
-      )}
+        );
+      }}
     </Fetcher>
-  )
+  );
 }
 
 export default TransferTypes;
