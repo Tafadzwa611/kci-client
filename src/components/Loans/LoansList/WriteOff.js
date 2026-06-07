f65a4e539268c76534a43bdc016ab6d34d4e25ff
Tafadzwa Kuno <tafadzwakuno@gmail.com>
2026-06-07 22:27:08 +0200

    feat: refactor WriteOff component to use Modal and update form submission logic for write-off reason

--- src/components/Loans/LoansList/WriteOff.js
+++ src/components/Loans/LoansList/WriteOff.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { ActionModal, ActionModalDialog, NonFieldErrors } from '../../../common';
+import {
+  Modal,
+  CustomTextField,
+  ModalSubmit,
+  NonFieldErrors
+} from '../../../common';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { Form, Formik } from 'formik';
 
 function WriteOff({loanId, setOpen, setLoan, updateLoanList, setLoanData}) {
-  const onSubmit = async (_, actions) => {
+  const onSubmit = async (values, actions) => {
     try {
       const CONFIG = {headers: {'X-CSRFToken': Cookies.get('csrftoken'), 'Accept': 'application/json', 'Content-Type': 'application/json'}};
-      const response = await axios.patch(`/loansapi/write_off/${loanId}/`, {}, CONFIG);
+      const response = await axios.post(`/loansapi/write_off/${loanId}/`, values, CONFIG);
       const newLoan = response.data;
       setLoan(newLoan);
       setOpen(false);
@@ -27,22 +32,21 @@ function WriteOff({loanId, setOpen, setLoan, updateLoanList, setLoanData}) {
   }
 
   return (
-    <ActionModal open={true} setOpen={setOpen} title={'Write Off Loan'}>
-      <Formik initialValues={{expected_disbursement_date: ''}} onSubmit={onSubmit}>
+    <Modal open={true} setOpen={setOpen} title='Write-Off Loan'>
+      <Formik initialValues={{write_off_reason: ''}} onSubmit={onSubmit}>
         {({ errors, isSubmitting }) => (
           <Form>
-            <NonFieldErrors errors={errors}>
-              <ActionModalDialog 
-                isSubmitting={isSubmitting}
-                msg={'Are you sure you want to write off this loan.'}
-                setOpen={setOpen}
-                act={'Write Off'}
-              />
-            </NonFieldErrors>
+            <div className='create_modal_container'>
+              <div>
+                <CustomTextField label='Write Off Reason' name='write_off_reason' type='text' required/>
+                <NonFieldErrors errors={errors}/>
+              </div>
+              <ModalSubmit isSubmitting={isSubmitting} setOpen={setOpen}/>
+            </div>
           </Form>
         )}
       </Formik>
-    </ActionModal>
+    </Modal>
   )
 }
 
